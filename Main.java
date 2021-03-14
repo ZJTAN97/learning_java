2c8639397889f4552da3657d54269730c95203b6
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-03-14 23:34:01 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // primitive datatypes, immutable
+        int hello_world = 5;
+        double num2 = 5.0; // floating decimal point
+        boolean b = true; // boolean true or false
+        char c = '5'; // can only accept 1 character, single quotation mark
+
+        // not primitivate datatype
+        String str = "Trying to learn java and springboot.."; // double quotation marks, can accept more than 1 character
+
+        System.out.println(hello_world);
+        System.out.println(num2);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(str);
+    }
+}
