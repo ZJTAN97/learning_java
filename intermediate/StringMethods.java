dd9e8e176767c1b1c666be205441c312595eb939
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-06-15 23:11:47 +0800

    updates

---
+++ intermediate/StringMethods.java
@@ -0,0 +1,21 @@
+import java.text.NumberFormat.Style;
+
+public class StringMethods {
+    
+    public static void main(String args[]) {
+
+        String[] words = {"Test1", "Test2", "Test3"};
+
+        //startWith
+        for(String w : words) {
+            if(w.startsWith("Test"))
+                System.out.println(w + "starts with Test");
+        }
+
+        //endsWith
+        for(String w : words) {
+            if(w.endsWith("t1"))
+                System.out.println(w + "ends with t1");
+        }
+    }
+}
