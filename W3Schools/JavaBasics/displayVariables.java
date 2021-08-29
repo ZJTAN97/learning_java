c3a60ce64ba4ac901ad2974c56677e54578d8371
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-08-30 00:38:21 +0800

    learning methods

---
+++ W3Schools/JavaBasics/displayVariables.java
@@ -0,0 +1,13 @@
+public class displayVariables {
+    public static void main(String[] args) {
+        
+        String name = "John";
+        System.out.println("Hello " + name);
+
+        // Numerics
+        int x = 5;
+        int y = 6;
+        System.out.println(x+y);
+
+    }
+}
---
+++ W3Schools/JavaBasics/javaTypeCasting.java
@@ -0,0 +1,17 @@
+public class javaTypeCasting {
+    public static void main(String[] args) {
+        
+        // Widening casting
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println(myDouble);
+
+
+        // Narrow casting
+        double mySecondDouble = 9.6d;
+        int mySecondInt = (int) mySecondDouble;
+        System.out.print(mySecondInt);
+
+
+    }
+}
