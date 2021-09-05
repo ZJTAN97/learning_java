d30fdad6409c41e809127fd6b9bd7467cdac2f3c
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-09-05 23:50:18 +0800

    failed to solve intermediate qn..

--- Challenges/Intermediate/LetterBoxPaintSquad.java
+++ Challenges/Intermediate/LetterBoxPaintSquad.java
@@ -1,44 +1,30 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+
+//https://www.codewars.com/kata/597d75744f4190857a00008d/train/java
+// 7kyu Letterbox Paint-Squad
 
 public class LetterBoxPaintSquad {
 
-    public static void paintLetterBoxes(final int start, final int end) {
+    public static int[] paintLetterBoxes(final int start, final int end) {
 
-        List<Character> list = new ArrayList<Character>();
-        List<Number> results = new ArrayList<Number>();
-
-        // get range between start and end
-        for (int i=start; i < end+1; i++ ) {
-            String stringifiedNumber = String.valueOf(i);
-            for (int a=0; a < stringifiedNumber.length(); a++) {
-                list.add(stringifiedNumber.charAt(a));
+        int[] frequencies = new int[10];
+        for (int i=start; i<= end; i++) {
+            int num = i;
+            while(num > 0) {
+                frequencies[num%10]++;
+                num /= 10;
+                System.out.println(num);
             }
         }
 
-        Collections.sort(list);
+        // for(int i=0; i<frequencies.length; i++) {
+        //     System.out.println(frequencies[i]);
+        // }
 
-        for(int i=0; i<10; i++) {
-
-            switch(i) {
-                case "1": 
-            }
-
-        }
-
-        
-        
-        for(int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
-        }
- 
-
-
-
-        // calculate each frequency
-
-        // return in order
+        return frequencies;
     }
 
     public static void main(String[] args) {
