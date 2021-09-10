c98b22ae39dece14bf3ebacd7d89de1020d4dbdc
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-09-10 10:02:27 +0800

    learnt linkedlists and hashmap

---
+++ W3Schools/JavaClasses/LinkedListsConcept/Main.java
@@ -0,0 +1,14 @@
+package LinkedListsConcept;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Import the LinkedList class
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
