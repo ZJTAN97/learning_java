6e18d42ea672fe3af677fe7ca17e7bc3647e91df
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-09-13 00:46:20 +0800

    updates

--- W3Schools/JavaClasses/InterfaceConcept/Main.java
+++ W3Schools/JavaClasses/InterfaceConcept/Main.java
@@ -1,4 +1,4 @@
-package Interface;
+package InterfaceConcept;
 
 public class Main {
     public static void main(String[] args) {
--- W3Schools/JavaClasses/InterfaceConcept/Multiple/Main.java
+++ W3Schools/JavaClasses/InterfaceConcept/Multiple/Main.java
@@ -1,4 +1,4 @@
-package Interface.Multiple;
+package InterfaceConcept.Multiple;
 
 public class Main {
     public static void main(String[] args) {
