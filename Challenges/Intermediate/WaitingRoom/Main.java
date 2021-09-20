0c94d463c8f528195d878b1673538ec7e9d9c2b4
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-09-20 09:13:43 +0800

    added waitingRoom algorithm

--- Challenges/Intermediate/WaitingRoom/Main.java
+++ Challenges/Intermediate/WaitingRoom/Main.java
@@ -5,32 +5,65 @@ import java.util.*;
 public class Main {
     // https://www.codewars.com/kata/542f0c36d002f8cd8a0005e5/java
     public static void main(String[] args) {
-        lastPatientChairNumber(13);
+        System.out.println(lastChair(100));
+        // once you use it a few times, you realise
+        // the answer is just n - 1
     }
 
 
-    public static void lastPatientChairNumber(int numPatients) {
-        Integer[] patientsArr = new Integer[numPatients];
-        Integer[] chairsArr = new Integer[numPatients];
-        Arrays.fill(chairsArr, 0);
-        Arrays.setAll(patientsArr, i -> i + 1);
-        List<Integer> patientsList = Arrays.asList(patientsArr);
-        List<Integer> chairsList = Arrays.asList(chairsArr);
-
-        condition1(patientsList, 2, 5);
-
-        System.out.println(chairsList);
-
-    } 
-
-
-    public static void condition1(List patientsList, int patient1, int patient2) {
-        // find a index as far from other index as possible
-        
+    private static int getBeatSeat(boolean[] chairs) {
+        int index = -1;
+        int maxSoFar = -1;
+        for(int i=0; i < chairs.length; i++) {
+            if(!chairs[i]) {
+                int score = calcScore(chairs, i);
+                if (score > maxSoFar) {
+                    index = i;
+                    maxSoFar = score;
+                }
+            }
+        }
+        return index;
     }
 
-    public static void getDistanceFromExit() {
 
+    private static int calcScore(boolean[] chairs, int chair) {
+        int leftScore = 0;
+        if (chair == 0) {
+            leftScore = chairs.length;
+        } else {
+            for (int i = chair - 1; i >= 0; i--) {
+                if (chairs[i]) {
+                    break;
+                }
+                leftScore ++;
+            }
+        }
+
+        int rightScore = 0;
+        if (chair == chairs.length - 1) {
+          rightScore = chairs.length;
+        } else {
+          for (int i = chair + 1; i < chairs.length; i++) {
+            if (chairs[i]) {
+              break;
+            }
+            rightScore ++;
+          }
+        }
+        return (leftScore < rightScore) ? leftScore : rightScore;
     }
+
+    
+    public static int lastChair(int n) {
+        boolean[] chairs = new boolean[n];
+        int chair = 0;
+        for (int i = 0; i < n; i++) {
+            chair = getBeatSeat(chairs);
+            chairs[chair] = true;
+        }
+        return chair + 1;
+    }
+
 
 }
