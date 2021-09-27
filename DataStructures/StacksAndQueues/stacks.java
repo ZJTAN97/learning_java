6ec7038e73cd0eab1f9d2fff788ad453f486ebe5
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-09-27 11:07:21 +0800

    learnt about stacks and attempted 1 leetcode easy qn

---
+++ DataStructures/StacksAndQueues/stacks.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class stacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stackPush(stack);
+        stackPop(stack);
+        stackPush(stack);
+        stackPeek(stack);
+        stackSearch(stack, 2);
+        stackSearch(stack, 6);
+    }
+
+    // Pushing element on the top of the stack
+    static void stackPush(Stack<Integer> stack) {
+        for(int i=0; i<5; i++) {
+            stack.push(i);
+        }
+    }
+
+
+    // Popping element on the top of the stack
+    static void stackPop(Stack<Integer> stack) {
+        System.out.println("Pop Operation:");
+ 
+        for(int i = 0; i < 5; i++)
+        {
+            Integer y = (Integer) stack.pop();
+            System.out.println(y);
+        }
+    }
+
+    // Displaying element on the top of the stack
+    static void stackPeek(Stack<Integer> stack) {
+        Integer element = (Integer) stack.peek();
+        System.out.println("Element on stack top: " + element);
+    }
+     
+    // Searching element in the stack
+    static void stackSearch(Stack<Integer> stack, int element) {
+        Integer pos = (Integer) stack.search(element);
+ 
+        if(pos == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element is found at position: " + pos);
+    }
+
+}
