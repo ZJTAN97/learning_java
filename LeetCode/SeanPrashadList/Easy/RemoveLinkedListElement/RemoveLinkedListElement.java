585610e84e01ed05ccd9a7e15e9042f0f8207144
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-10-26 10:32:17 +0800

    attempted one leetcode question and revising linkedlist..

---
+++ LeetCode/SeanPrashadList/Easy/RemoveLinkedListElement/RemoveLinkedListElement.java
@@ -0,0 +1,64 @@
+public class RemoveLinkedListElement {
+
+
+    static ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+
+        while(current.next != null) {
+            if(current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+            // printList(current);
+            printList(dummy);
+            System.out.println("---");
+        }
+
+        // printList(dummy.next);
+
+        return dummy.next;
+
+    }
+
+
+    static void printList(ListNode head) {
+        ListNode current = head;
+        while(current != null) {
+            System.out.println(current.val);
+            current = current.next;
+        }
+    }
+    public static void main(String[] args) {
+
+
+        ListNode first = new ListNode(2);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(1);
+        ListNode fourth = new ListNode(2);
+        // ListNode fifth = new ListNode(2);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        // fourth.next = fifth;
+
+        removeElements(first, 1);
+
+    }
+}
+
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode (int x) {
+        this.val = x;
+        next = null;
+    }
+}
