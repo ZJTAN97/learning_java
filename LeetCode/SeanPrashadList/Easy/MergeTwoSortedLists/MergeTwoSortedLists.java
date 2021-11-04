a8d264da6a22d5a2713165eea9a44473e384f5f7
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-11-04 09:27:17 +0800

    revised on mergetwosortedlists and did binary search qn

---
+++ LeetCode/SeanPrashadList/Easy/MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -0,0 +1,74 @@
+package SeanPrashadList.Easy.MergeTwoSortedLists;
+
+public class MergeTwoSortedLists {
+
+    static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode returnNode = new ListNode(0);
+        ListNode current = returnNode;
+
+
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+
+            current = current.next;
+        }
+
+        if(l1 != null) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+
+        printList(returnNode.next);
+
+        return returnNode.next;
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(1);
+        ListNode fifth = new ListNode(2);
+        ListNode sixth = new ListNode(3);
+
+
+        first.next = second;
+        second.next = third;
+        
+        fourth.next = fifth;
+        fifth.next = sixth;
+
+        mergeTwoLists(first, fourth);
+
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode (int x) {
+        this.val = x;
+        next = null;
+    }
+}
+
