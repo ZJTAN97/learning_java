7b8603c1f685cf2c2b9c755f8de80aaf7cc476f1
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-03-04 15:32:55 +0800

    starting new bobok

---
+++ DataStructures/src/Arrays/evenOdd.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class evenOdd {
+    
+    public void sort(int[] A) {
+        int nextEven = 0, nextOdd = A.length - 1;
+        while(nextEven < nextOdd) {
+            if(A[nextEven] % 2 == 0) {
+                nextEven ++;
+            } else {
+                int temp  = A[nextEven];
+                A[nextEven] = A[nextOdd];
+                A[nextOdd--] = temp; 
+            }
+        }
+        System.out.println(Arrays.toString(A));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 2, 5, 8, 9, 4};
+        evenOdd sorter = new evenOdd();
+        sorter.sort(arr);
+    }
+}
+
+// Array Question Page 60
